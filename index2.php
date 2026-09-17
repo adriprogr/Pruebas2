b56bd1a85c64770424e1372faa3b8a127f53cf1b
adriprogr <adrian19379393@gmail.com>
2026-09-17 18:32:17 +0200

    Prueba

---
+++ index2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Hola, solo, bienvenido al php en 2026
+    <br>
+    <?php
+        $nombre = "Adrian";
+        echo "Hola " . $nombre;
+         
+    ?>
+</body>
+</html>
